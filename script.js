5af3b02a1ba47bbc103b0c4ee5f579f68f54d3b2
axldimai <axelelprogramador@gmail.com>
2023-06-21 19:20:43 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function showAlert() { alert( 'hola, esta es una alerta de javascript' ); }
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
